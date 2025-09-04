69dd5efc967d271653dc6a9f9aa532115ca292aa
daisy <daisy.kang@planby.us>
2025-09-04 20:55:18 +0900

    favicon 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
-
+import type { Metadata } from "next";
 import "./globals.css";
 
 import { UserProvider } from "@/context/UserContext";
+
+export const metadata: Metadata = {
+    title: `EEVEE RUN!`,
+    description: "mini game",
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon.ico",
+        apple: "/favicon.png",
+    },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
